0a91517363158eacdbb9597e6ae8451026d5661f
tungduong <tungduong1309@gmail.com>
2025-01-11 23:12:46 +0700

    Update main.py
    import write functions from input
    update choice-handling codes

--- pw5/main.py
+++ pw5/main.py
@@ -1,5 +1,5 @@
 from domains.management import Management
-from input import student_input, course_input, mark_input
+from input import *
 from output import list_students, list_courses, list_marks 
 import os
 def main():
@@ -16,21 +16,34 @@ def main():
                     print("\nAdding students' info...")
                     student=student_input(existing_ids=menu.students.keys())
                     menu.add_stu(student)
+                    write_students(menu.students)
                 case 2:
                     print("\nAdding courses' info...")
                     course=course_input(existing_ids=menu.courses.keys())
                     menu.add_course(course)
+                    write_courses(menu.courses)
                 case 3:
                     print("\nAdding marks...")
                     course_id=input("Enter the course id: ")
                     if course_id not in menu.courses.keys():
                         print("Course ID's not found")
+                        continue
+                    if not menu.students.keys():
+                        print("No students in the system")
+                        continue
                     marks=mark_input(course_id,menu.students)
                     menu.add_mark(marks)
+                    write_marks(menu.marks,menu.students)
                 case 4:
-                    print("\nListing all courses...")
+                    if not menu.courses.keys():
+                        print("\nNo courses in the system yet")
+                        continue
+                    print("\nListing all courses...")                    
                     list_courses(menu.courses)
                 case 5:
+                    if not menu.students.keys():
+                        print("\nNo students in the system yet")
+                        continue
                     print("\nListing all students...")
                     menu.calculating_gpa()
                     sorted=menu.list_students()
